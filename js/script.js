b9718cda453a1726cf7f3008d38cafe0d30258e9
barrosraffa <rafaellaponci@gmail.com>
2019-05-23 23:25:29 -0300

    adicionei arquivos sass

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
-var slider = tns({
-   container: '.my-slider',
-   items: 1,
-   slideBy: 'page',
-  //  autoplay: true,
-   arrowKeys: true,
-   controlsContainer: '.navegation',
-   nav: false,
- });
+// <!-- START PART TWO HOME -->
+const cards = document.getElementsByClassName("cards") // pega os cards do html
+const cardsArray = Array.from(cards) // transforma em uma list [card1, card2, card3]
+
+
+cardsArray.forEach(element => {
+    //pega os cards na lista um de cada vez
+
+    //adiciona a classe card__active quando coloca mouse em cima do card
+    element.addEventListener("mouseover", function () {
+        element.classList.add("cards__active")
+    })
+
+    //remove a classe card__active quando remove o mouse de cima do card
+    element.addEventListener("mouseout", function () {
+        element.classList.remove("cards__active")
+    })
+})
+ // <!-- END PART TWO HOME -->
+
+// <!-- START PART THREE HOME -->
+let slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    arrowKeys: true,
+    controlsContainer: '.arrow__navegation',
+    nav: false,
+  });
+// <!-- END PART THREE HOME -->
